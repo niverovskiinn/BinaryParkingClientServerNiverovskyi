edf1a593fc5bcc6440dfc8b4e108db199faeb079
Never04eK <heket844@gmail.com>
2019-05-24 14:25:43 +0300

    Delete useful Vehicle's prop

--- Models/Vehicle/Vehicle.cs
+++ Models/Vehicle/Vehicle.cs
@@ -6,11 +6,8 @@ namespace BinaryParkingClientServerNiverovskyi.Models.Vehicle
     public class Vehicle
     {
         [JsonProperty("id")] public int Id { get; set; }
-        [JsonProperty("isparked")] public bool IsParked { get; set; }
-        [JsonProperty("plate")] public string Plate { get; set; }
         [JsonProperty("balance")] public double Balance { get; set; }
         [JsonProperty("lastpaidtime")] public DateTime LastPaidTime { get; set; }
-        [JsonProperty("model")] public string Model { get; set; }
         [JsonProperty("typeofvehicle")] public VehicleType TypeOfVehicle { get; set; }
     }
 }
