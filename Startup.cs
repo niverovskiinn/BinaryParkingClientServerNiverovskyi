d70f6d31dc27d60ace51a165961dadcc7428ac13
Never04eK <heket844@gmail.com>
2019-05-24 13:05:49 +0300

    Update project files

--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using BinaryParkingClientServerNiverovskyi.Models.Transaction;
+using BinaryParkingClientServerNiverovskyi.Models.Vehicle;
+
 
 namespace BinaryParkingClientServerNiverovskyi
 {
@@ -22,13 +19,21 @@ namespace BinaryParkingClientServerNiverovskyi
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the 
+        //container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //TODO parking
+//            services.AddSingleton();
+            services.AddDbContext<VehicleContext>(opt =>
+                opt.UseInMemoryDatabase("VehicleList"));
+            services.AddDbContext<TransactionContext>(opt =>
+                opt.UseInMemoryDatabase("TransactionList"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime. Use this method to configure the HTTP 
+        //request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -37,12 +42,19 @@ namespace BinaryParkingClientServerNiverovskyi
             }
             else
             {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                // The default HSTS value is 30 days. You may want to change this for 
+                // production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
-            app.UseMvc();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute("default", "{controller}/{action}/{id}");
+                routes.MapRoute("default2", "{controller}/{action}");
+                routes.MapRoute("default3",
+                    "{controller}/{action}/{id}-{sum}"); //-{lastPaidTime}-{typeV}");
+            });
         }
     }
 }
